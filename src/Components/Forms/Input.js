d04e46eb63defd010133f53488d5d8d3c2601cb0
Victor Rocha <vrsarmento@gmail.com>
2021-06-03 02:11:19 -0300

    Added new props to Input component

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styles from './Input.module.css'
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, error, onChange, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input className={styles.input} type={type} id={name} name={name} />
-      <p className={styles.error}>Erro</p>
+      <input
+        className={styles.input}
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   )
 }

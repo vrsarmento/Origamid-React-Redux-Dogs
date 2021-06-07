6f811163e98eca458e04cd39dc1756ce9269d765
Victor Rocha <vrsarmento@gmail.com>
2021-06-07 19:37:35 -0300

    userContext's config in App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Home from './Components/Home'
 import Login from './Components/Login'
 import Footer from './Components/Footer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { UserStorage } from './Contexts/UserContext'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login/*' element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login/*' element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   )

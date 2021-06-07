f0d4b4f4bc1b1d3737635e8d82a7c79e5eebabbf
Victor Rocha <vrsarmento@gmail.com>
2021-06-07 19:39:03 -0300

    Login main

---
+++ src/Components/Login/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
+import UserContext from '../../Contexts/UserContext'
+
+const Login = () => {
+  const { login } = React.useContext(UserContext)
+
+  if (login === true) return <Navigate to='/conta' />
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='criar' element={<LoginCreate />} />
+        <Route path='perdeu' element={<LoginPasswordLost />} />
+        <Route path='resetar' element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default Login

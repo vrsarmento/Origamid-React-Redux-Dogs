09e46aa75e766fbad185bfd095d49672cdc1cd21
Victor Rocha <vrsarmento@gmail.com>
2021-06-07 19:35:56 -0300

    Added user's infos and actions in Header using userContext

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,17 +2,28 @@ import React from 'react'
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Dogs } from '../../Assets/dogs.svg'
+import { UserContext } from '../../Contexts/UserContext'
 
 const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext)
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
           <Dogs />
         </Link>
-        <Link className={styles.login} to='/login'>
-          Login / Criar Conta
-        </Link>
+
+        {data ? (
+          <Link className={styles.login} to='/conta'>
+            {data.nome}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to='/login'>
+            Login / Criar Conta
+          </Link>
+        )}
       </nav>
     </header>
   )

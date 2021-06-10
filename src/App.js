79c335ff05f9c037c65b528d271d3b005a5cc02d
Victor Rocha <vrsarmento@gmail.com>
2021-06-10 18:33:02 -0300

    Added ProtectedRoute for user account in Routes list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Login from './Components/Login'
 import Footer from './Components/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import User from './Components/User'
+import ProtectedRoute from './Components/Helpers/ProtectedRoute'
 import { UserStorage } from './Contexts/UserContext'
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
           <Header />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/login/*' element={<Login />} />
+            <Route path='login/*' element={<Login />} />
+            <ProtectedRoute path='conta/*' element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>

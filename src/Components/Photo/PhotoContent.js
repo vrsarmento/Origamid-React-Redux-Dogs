031c8224fbc4d2fcbc390818029d5b11fbc7eb8e
Victor Rocha <vrsarmento@gmail.com>
2021-06-18 21:23:00 -0300

    Implementing PhotoDelete button | endpoint PHOTO_DELETE

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import UserContext from '../../Contexts/UserContext'
 import PhotoComments from './PhotoComments'
 import styles from './PhotoContent.module.css'
+import PhotoDelete from './PhotoDelete'
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext)
   const { photo, comments } = data
 
   return (
@@ -13,10 +16,14 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <div className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.views}>{photo.acessos}</span>
-          </p>
+          </div>
           <h1 className='title'>
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { PHOTO_DELETE } from '../../api'
+import useFetch from '../../Hooks/useFetch'
+import styles from './PhotoDelete.module.css'
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch()
+  const token = window.localStorage.getItem('Dogs_Token')
+
+  async function handleClick() {
+    const confirm = window.confirm('Tem certeza que deseja apagar a foto?')
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id, token)
+      const response = await request(url, options)
+      if (response.response.ok) window.location.reload()
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
+  )
+}
+
+export default PhotoDelete

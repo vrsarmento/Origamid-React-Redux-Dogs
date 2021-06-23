74f4ea807917499f570b6d7c3a1016096817f659
Victor Rocha <vrsarmento@gmail.com>
2021-06-23 19:32:56 -0300

    Setting PropTypes to Feed's props

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos'
 
@@ -52,6 +53,17 @@ const Feed = ({ user }) => {
       ))}
     </div>
   )
+}
+
+Feed.defaultProps = {
+  user: 0
+}
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired
+  ])
 }
 
 export default Feed

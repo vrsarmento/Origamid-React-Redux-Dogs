2bd3387309d9a7d564edde475c1afde43687748e
Victor Rocha <vrsarmento@gmail.com>
2021-07-30 19:08:37 -0300

    CSS minor adjusts || .htaccess config

--- src/Components/Helpers/Error.js
+++ src/Components/Helpers/Error.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Error.module.css'
 
 const Error = ({ error }) => {
   if (!error) return null
 
-  return <p style={{ color: '#f31', margin: '1rem 0' }}>{error}</p>
+  return <p className={styles.error}>{error}</p>
 }
 
 export default Error

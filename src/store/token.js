dfb883db1d5aacf740d549ab5ef38f98237090f5
Victor Rocha <vrsarmento@gmail.com>
2021-08-02 20:30:25 -0300

    Implementing userLogout and autoLogin

--- src/store/token.js
+++ src/store/token.js
@@ -1,11 +1,19 @@
 import { TOKEN_POST } from '../api'
+import { config } from '../config'
 import createAsyncSlice from './helpers/createAsyncSlice'
 
 const slice = createAsyncSlice({
   name: 'token',
+  initialState: {
+    data: {
+      token:
+        window.localStorage.getItem(config.LOCALSTORAGE_TOKEN_KEY_ID) || null
+    }
+  },
   fetchConfig: (user) => TOKEN_POST(user)
 })
 
 export const fetchToken = slice.asyncAction
+export const { resetState: resetTokenState } = slice.actions
 
 export default slice.reducer
--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,7 @@
 import { USER_GET } from '../api'
 import createAsyncSlice from './helpers/createAsyncSlice'
-import { fetchToken } from './token'
+import { fetchToken, resetTokenState } from './token'
+import { config } from '../config'
 
 const slice = createAsyncSlice({
   name: 'user',
@@ -8,10 +9,30 @@ const slice = createAsyncSlice({
 })
 
 export const fetchUser = slice.asyncAction
+const { resetState: resetUserState, fetchError } = slice.actions
 
 export const userLogin = (user) => async (dispatch) => {
   const { payload } = await dispatch(fetchToken(user))
-  if (payload.token) await dispatch(fetchUser(payload.token))
+  if (payload.token) {
+    await dispatch(fetchUser(payload.token))
+    window.localStorage.setItem(config.LOCALSTORAGE_TOKEN_KEY_ID, payload.token)
+  }
+}
+
+export const userLogout = () => async (dispatch) => {
+  dispatch(resetUserState())
+  dispatch(resetTokenState())
+  window.localStorage.removeItem(config.LOCALSTORAGE_TOKEN_KEY_ID)
+}
+
+export const autoLogin = () => async (dispatch, getState) => {
+  const { token } = getState()
+  if (token?.data?.token) {
+    const { type } = await dispatch(fetchUser(token.data.token))
+    if (type === fetchError.type) {
+      dispatch(userLogout())
+    }
+  }
 }
 
 export default slice.reducer

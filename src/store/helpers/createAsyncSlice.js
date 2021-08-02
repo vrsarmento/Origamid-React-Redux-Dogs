af7bed4972b096bcb837fa9ca4d75eeb9829182e
Victor Rocha <vrsarmento@gmail.com>
2021-08-02 19:12:50 -0300

    Implementing createAsyncSlice helper

---
+++ src/store/helpers/createAsyncSlice.js
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+/**
+ * Cria um slice com uma função assíncrona
+ * @param {Object} config
+ * @param {String} config.name
+ * @param {Object} config.initialState
+ * @param {Object} config.reducers
+ * @param {Function} config.fetchConfig
+ */
+const createAsyncSlice = (config) => {
+  const slice = createSlice({
+    name: config.name,
+    initialState: {
+      loading: false,
+      data: null,
+      error: null,
+      ...config.initialState
+    },
+    reducers: {
+      fetchStarted(state) {
+        state.loading = true
+      },
+      fetchSuccess(state, action) {
+        state.loading = false
+        state.data = action.payload
+        state.error = null
+      },
+      fetchError(state, action) {
+        state.loading = false
+        state.data = null
+        state.error = action.payload
+      },
+      ...config.reducers
+    }
+  })
+
+  // desestruturação das ações
+  const { fetchStarted, fetchSuccess, fetchError } = slice.actions
+  // ação assíncrona (thunk), recebe um payload
+  const asyncAction = (payload) => async (dispatch) => {
+    try {
+      dispatch(fetchStarted())
+      // config.fetchConfig é um método que retorna
+      // o url e as opções do fetch
+      const { url, options } = config.fetchConfig(payload)
+      const response = await fetch(url, options)
+      const data = await response.json()
+      if (response.ok === false) throw new Error(data.message)
+      return dispatch(fetchSuccess(data))
+    } catch (error) {
+      return dispatch(fetchError(error.message))
+    }
+  }
+
+  // a função retorna as propriedades de slice e a ação assíncrona
+  return { ...slice, asyncAction }
+}
+
+export default createAsyncSlice
